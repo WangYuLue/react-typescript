0e5191e712afe9b7aec376c1148f0c02ca318452
wangyulue <1206180097@qq.com>
2019-08-30 19:10:14 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test : /\.tsx?$/,
+        use : 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    port: 3000
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      "@container": path.resolve(__dirname, 'src/container/')
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template : './index.html'
+    })
+  ]
+}
