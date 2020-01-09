c2b6cf71bebd9fa229ac828a5e544549c80a516a
wangyulue <1206180097@qq.com>
2020-01-09 17:04:46 +0800

    feat(*)add eslint

--- src/container/App/index.tsx
+++ src/container/App/index.tsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import './index.scss'
+import './index.scss';
 class App extends Component {
   render() {
     return (
       <div>Hello React!</div>
-    )
+    );
   }
 }
 
